20b8c5b000a1c107dd383abef8b7f7320b2ad337
Nguyen Khang <19521663@gm.uit.edu.vn>
2022-10-29 11:49:49 +0700

    add template

--- src/components/user/footer.js
+++ src/components/user/footer.js
@@ -1,9 +1,9 @@
 import React from "react";
 
-export const Footer=() => {
-    return <>
-   <div className="h-[100px] bg-slate-400">
-    đây là footer
-   </div>
+export const Footer = () => {
+  return (
+    <>
+      <div className="h-[100px] bg-slate-400">đây là footer</div>
     </>
-}
+  );
+};
--- src/layout/userLayout.js
+++ src/layout/userLayout.js
@@ -6,20 +6,18 @@ import { Header } from "../components/user/header";
 import { Footer } from "../components/user/footer";
 
 export const Layout = () => {
-    return <>
-    <div className="bg-[#f5f5f5] min-h-screen flex flex-col justify-between">
-    <div>
-    <Header/>
+  return (
+    <>
+      <div className="bg-[#f5f5f5] min-h-screen flex flex-col justify-between">
+        <div>
+          <Header />
 
-    <div className="" >
-        <Outlet></Outlet>
-    </div>
-    </div>
-    
-
-    <Footer className=""/>
-    
-    </div>
-    
+          <div className="">
+            <Outlet></Outlet>
+         </div>
+        </div>
+        <Footer className="" />
+      </div>
     </>
+  );
 }
